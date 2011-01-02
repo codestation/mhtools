2f84945b33c740343c42c42b28d1e544bda70a06
codestation <cesarg9@gmail.com>
2011-01-02 19:48:30 +0000

    Added support for data install in the creation of patchfiles

--- src/base/PatchBuilder.java
+++ src/base/PatchBuilder.java
@@ -1,15 +1,19 @@
 package base;
 
+import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Vector;
 
 import crypt.DecryptTable;
 
@@ -47,11 +51,71 @@ public class PatchBuilder extends DecryptTable {
                 out.seek(current);                
             }
             writeInt(out, 0);
+            install_tables(out, list);
             out.close();
         }catch(FileNotFoundException e) {
             e.printStackTrace();
         } catch (EOFException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void install_tables(RandomAccessFile out, List<String> list) {
+        System.out.print("Trying to add data_install tables...");
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("data_install.txt"));
+            out.seek(out.length());
+            
+            String line;
+            Vector<Integer> patch_files = new Vector<Integer>();
+            Vector<String> install_files = new Vector<String>();
+            Vector<String> offsets = new Vector<String>();
+            
+            while((line = in.readLine()) != null) {
+                if(!line.startsWith("#")) {
+                    String []tokens = line.split(",");
+                    patch_files.add(Integer.parseInt(tokens[0]));
+                    install_files.add(tokens[1]);
+                    offsets.add(tokens[2]);
+                }
+            }
+            Vector<String> install_uniq = new Vector<String>(new LinkedHashSet<String>(install_files));
+            writeInt(out, install_uniq.size());
+            int install_count = 0;
+            String match = install_files.firstElement();
+            out.write(match.getBytes());
+            writeInt(out, install_count);
+            for(String file : install_files) {
+                if(!match.equals(file)) {
+                    out.write(file.getBytes());
+                    match = file;
+                    writeInt(out, install_count);
+                }
+                install_count++;
+            }
+            for(String trans_file : list) {
+                int index = patch_files.indexOf(extractNumber(trans_file));
+                int offset = -1;
+                if(index != -1) {
+                    // UGH!!, 11 years and the Integer.parseInt bug isn't fixed
+                    // This is the last time that i use Java in a project of mine
+                    // http://bugs.sun.com/view_bug.do?bug_id=4215269
+                    offset = (int)Long.parseLong(offsets.get(index),16);
+                } else {
+                    System.out.println("Install info not found for " + trans_file);
+                }
+                writeInt(out, offset);
+            }            
+            long filelength = out.length();
+            if(filelength % 16 > 0) {
+                filelength += 16 - (filelength % 16);
+                out.setLength(filelength);
+            }
+            System.out.println("OK");
+        } catch (FileNotFoundException e) {
+            System.out.println("\ndata_install.txt not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
